494290ea51bf42ae77ec767f8b6e981536ea3978
WaffleBuffer <medard.thomas@outlook.fr>
2019-02-18 14:49:12 +0100

    fixed delete

--- src/pages/todo-items/todo-items.ts
+++ src/pages/todo-items/todo-items.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Constants} from "../../app/utilities/Constants";
 import { TodoListItemCreationPage } from '../todo-list-item-creation/todo-list-item-creation';
 import { ShareListPage } from '../share-list/share-list';
+import { Observable } from 'rxjs';
+import { TodoList } from '../../app/TodoList/model/model';
+import { TodoServiceProvider } from '../../providers/todo/todo-serviceProvider';
 
 @IonicPage()
 @Component({
@@ -12,14 +15,28 @@ import { ShareListPage } from '../share-list/share-list';
 export class TodoItemsPage {
 
   private listId?:string;
+  private list$?: Observable<TodoList>
+  private list: TodoList
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log('constructing todo item page');  
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private todoListProvider: TodoServiceProvider) {
+    console.log('constructing todo item page');
 
-  ionViewDidLoad() {
     this.listId = this.navParams.get(Constants.TODOITEM_ID_NAME);
-    console.log('ionViewDidLoad TodoItemsPage id=' + this.listId);
+
+    // Get notified on list change
+    this.todoListProvider.getTodoListsSub().subscribe(lists => {
+
+      
+      this.list$ = this.todoListProvider.getList(this.listId);
+      this.list$.subscribe(list => {
+        this.list = list;
+        console.log('list = ' + JSON.stringify(this.list));
+      });
+    })
+  }
+  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TodoItemsPage id=' + this.listId + '; list$=' + JSON.stringify(this.list$));
   }
 
   public createListItemCommand() {
